871a96e3456da2ad63df32cad3866c78a9d00b23
IvanGCh <38388232+IvanGCh@users.noreply.github.com>
2019-03-11 13:34:10 -0600

    Update f_PriorDC.m

--- Code/f_PriorDC.m
+++ Code/f_PriorDC.m
@@ -15,8 +15,8 @@ function f_PriorDC(hObject, eventdata, handles)
     fontesc12=round(12*sqrt(respix(3)^2+respix(4)^2)/2202);
     
 	if kreg>1
-        kreg=kreg-1;
-    end
+          kreg=kreg-1;
+        end
     
     uicontrol('Style','text','String',[num2str(kreg) ' / ',num2str(nsac) , '  file' ],'background','w','FontSize',12,'Units','normalized','Position',[0.3 0.95 0.05 0.03])  
     
@@ -61,27 +61,9 @@ function f_PriorDC(hObject, eventdata, handles)
         maxlagsel=traceHDR.E;
         time=traceHDR.B:dt:traceHDR.E;
     end
-%     if strcmp(filetype,'correlogram')==1        
-%         maxtime=(dt*length(trace)-dt)/2;
-%         time=-maxtime:dt:maxtime;           time=round(time.*1000)./1000;
-%         tcut = -maxlagsel:dt:maxlagsel;
-%         for k1=1:length(time)
-%             if round(time(k1)*1000)==round(tcut(1)*1000)
-%                 ind1=round(k1);
-%                 ind2=ind1+2*round(maxlagsel/dt);
-%             end
-%         end
-%         trace=trace(ind1:ind2);   
-%         trace=f_Taper(trace,0.005);      
-%         time=time(ind1:ind2);
-%     
-%     elseif strcmp(filetype,'seismic_record')==1        
-%         maxlagsel=traceHDR.E;
-%         time=traceHDR.B:dt:traceHDR.E;
-%     end
     
     subplot(3,4,[3,7]),cla
-	subplot(3,4,[4,8]),cla
+    subplot(3,4,[4,8]),cla
     
     subplot(3,4,9:10),hold off
     plot(time,trace,'k'),axis([min(time) max(time) -max(abs(trace)) max(abs(trace))])
@@ -96,7 +78,6 @@ function f_PriorDC(hObject, eventdata, handles)
     end
     title(['\bf FILE`S NAME: ',char(namesacfile), ],'fontsize',fontesc12)
     
-    %%
     Tmin= evalin('base', 'Tmin');
     Tmax= evalin('base', 'Tmax');    
     filt= evalin('base', 'filt');
@@ -110,7 +91,7 @@ function f_PriorDC(hObject, eventdata, handles)
         f_Disp4PickCD_corr(time,fcm,T,FTAN,ENV,trace,dist,dt,maxlagsel);
         
         uicontrol('Style','text','String',' PART OF CC TO PICK ','background','w','FontSize',fontesc10,'Units','normalized','Position',[0.5 0.955 0.1 0.03])  
-        uicontrol('Style','popup','String', 'CONTINUOUS CAUSAL|CONTINUOUS NON-CAUSAL|SEGMENTED  CAUSAL|SEGMENTED  NON-CAUSAL','FontSize',fontesc10,'Units','normalized','Position', [0.6 0.935 0.10 0.05],'Callback', @f_SelectCausal2);   
+        uicontrol('Style','popup','String', 'CONTINUOUS CAUSAL|CONTINUOUS ANTICAUSAL|SEGMENTED  CAUSAL|SEGMENTED  ANTICAUSAL','FontSize',fontesc10,'Units','normalized','Position', [0.6 0.935 0.10 0.05],'Callback', @f_SelectCausal2);   
 
         uicontrol('Style','pushbutton','String','SAVE CURVE','FontSize',fontesc10,'Callback',{@f_SaveDC},'Units', 'normalized','Position', [0.8,0.96,0.08,0.03]);
         uicontrol('Style','pushbutton','String','EXPORT ALL CURVES','FontSize',fontesc10,'Callback',{@f_ExportDC},'Units', 'normalized','Position', [0.89,0.96,0.1,0.03]);
